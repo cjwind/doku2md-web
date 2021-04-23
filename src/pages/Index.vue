719fea28351b1f4d1cb184073d3b262fbe9ca5bf
cjw <cwentsai@gmail.com>
2021-04-23 23:42:35 +0800

    Convert many syntax

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -31,7 +31,14 @@ export default {
       let lines = doku.split('\n')
       let markdown = ''
       lines.forEach(line => {
-        markdown += this.convertHeader(line) + '\n'
+        line = this.convertHeader(line)
+        line = this.convertMonospaced(line)
+        line = this.convertCodeTag(line)
+        line = this.convertLink(line)
+        line = this.convertUnorderedListItem(line)
+        line = this.convertOrderedListItem(line)
+        line = this.convertItalic(line)
+        markdown += line + '\n'
       })
       return markdown
     },
@@ -43,9 +50,31 @@ export default {
         .replace('== ', '##### ')
         .replace('= ', '###### ')
     },
+    convertMonospaced (line) {
+      return line.replace(/''%%(.*)%%''/, '`$1`')
+    },
+    convertCodeTag (line) {
+      return line.replace(/<code( *)(.*)>/, "```$2")
+        .replace(/<\/code>/, '```')
+        .replace(/<sxh( *)(.*)>/, '```$2')
+        .replace(/<\/sxh>/, '```')
+    },
+    convertLink(line) {
+      return line.replace(/\[\[(.*)\|(.*)]]/, '[$2]($1)')
+    },
+    convertUnorderedListItem(line) {
+      return line.replace(/^  ( *)\* (.*)/, '* $2')
+    },
+    convertOrderedListItem(line){
+      return line.replace(/^  ( *)- (.*)/, '1. $2')
+    },
+    convertItalic(line){
+      return line.replace(/(http(s*):)\/\//, '__$1_placeholder__')
+        .replace(/\/\/(.*)\/\//, '*$1*')
+        .replace(/__(http(s*):)_placeholder__/, '$1//')
+    },
     onInput () {
       this.markdown = this.convert(this.text)
-      console.log(this.markdown)
     }
   }
 }
